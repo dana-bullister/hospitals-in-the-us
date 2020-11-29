ec0f7d898d3252d7515c9236a5833c018d3538d5
Dana Bullister <dana.bullister@gmail.com>
2020-11-29 07:34:26 -0500

    clean and comment script file

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function parseCSV(d) {
+// parse function for hospitals.csv data file: given a row instance, return a
+// dictionary of attribute values for that instance
+function parseCsv(d) {
   return {
     name: d["NAME"],
     lat: +d.LATITUDE,
@@ -9,49 +11,54 @@ function parseCSV(d) {
   };
 }
 
+// array of promises comprised of reading in data files
 const promises = [
-  d3.csv("./data/hospitals.csv", parseCSV),
+  d3.csv("./data/hospitals.csv", parseCsv),
   d3.json("./geojson/gz_2010_us_040_00_20m.json")
 ];
 
-
+// read in data files
 Promise.all(promises).then(function(data) {
 
-  const museums = data[0];
+  // store the data as a variables
+  const hospitals = data[0];
+  const geoData = data[1];
 
-  console.log(museums);
-
-  const world = data[1];
-
+  // store values for chart dimensions
   const width = document.querySelector("#chart").clientWidth;
   const height = document.querySelector("#chart").clientHeight;
+
+  // add chart svg to appropriate div with the same dimensions
   const svg = d3.select("#chart")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-
+  // configure geoData projection and presentation
   const projection = d3.geoMercator()
     .translate([width / 2, height / 2])
     .center([-83, 35])
     .scale(800);
 
+  // visually render the geoData
   const path = d3.geoPath()
     .projection(projection);
 
   svg.selectAll("path")
-    .data(world.features)
+    .data(geoData.features)
     .enter()
     .append("path")
     .attr("d", path)
     .attr("class", "state");
 
+  // set color palette for points on map
   const colorScale = d3.scaleOrdinal(d3.schemeSet1)
     .domain(["LONG TERM CARE", "GENERAL ACUTE CARE", "PSYCHIATRIC", "CRITICAL ACCESS", "REHABILITATION", "CHILDREN", "MILITARY", "WOMEN", "SPECIAL", "CHRONIC DISEASE"]);
 
-
+  // bind hospital locations to data points rendered on the map, colored by
+  // hospital type
   const points = svg.selectAll("circle")
-    .data(museums)
+    .data(hospitals)
     .enter().append("circle")
     .attr("cx", function(d) {
       var proj = projection([d.lon, d.lat]);
@@ -64,15 +71,17 @@ Promise.all(promises).then(function(data) {
       return colorScale(d.type);
     })
 
-
+  // add a tooltip to the chart
   const tooltip = d3.select("#chart")
     .append("div")
     .attr("class", "tooltip");
 
+  // set the tooltip to render in the general location of a given mapped data
+  // point upon mouseover
   svg.selectAll("circle")
     .on("mouseover", function(e, d) {
 
-      let cx = +d3.select(this).attr("cx") * k + tX + 20;
+      let cx = +d3.select(this).attr("cx") * k + tX + 20; // shift the tooltip slightly to make it easier to see data point
       let cy = +d3.select(this).attr("cy") * k + tY - 10;
 
       tooltip.style("visibility", "visible")
@@ -80,7 +89,7 @@ Promise.all(promises).then(function(data) {
         .style("top", cy + "px")
         .html(`<b>${d.name}</b><br>Type: ${d.type}<br>Location: ${d.city}, ${d.state}`);
 
-      d3.select(this)
+      d3.select(this) // and make the data point on the map increase in radius size
         .attr("r", 10 / k)
         .attr("stroke", "white")
         .attr("stroke-width", 2 / k);
@@ -92,25 +101,18 @@ Promise.all(promises).then(function(data) {
       d3.select(this)
         .attr("r", 3 / k)
         .attr("stroke", "none");
-
-
     });
 
-
-  /* FILTER BY CHECKBOX
-
-  We'll use the same filtering pattern we've seen before
-  to filter the markers on the map by museum type.
-
-  */
-
+  // filter the points rendered on the map by hospital type
+  // (do this by either hiding or showing the relevant set of points upon
+  // click of a filtering checkbox)
   d3.selectAll(".type--option").on("click", function() {
 
     let isChecked = d3.select(this).property("checked");
-    let museumType = d3.select(this).property("value");
+    let hospitalType = d3.select(this).property("value");
 
     let selection = points.filter(function(d) {
-      return d.type === museumType;
+      return d.type === hospitalType;
     });
 
     if (isChecked == true) {
@@ -121,7 +123,7 @@ Promise.all(promises).then(function(data) {
 
   });
 
-
+  //fff
   /*
   ADDING ZOOM
 
