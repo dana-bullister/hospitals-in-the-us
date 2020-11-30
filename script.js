66cccc64ae3c4709866e8b3c2e27851b14ebdda1
Dana Bullister <dana.bullister@gmail.com>
2020-11-30 18:25:15 -0500

    make points more translucent

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ Promise.all(promises).then(function(data) {
     .attr("fill", function(d) {
       return colorScale(d.type);
     })
+    .attr("fill-opacity", .5)
 
   // add a tooltip to the chart
   const tooltip = d3.select("#chart")
